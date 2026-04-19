314dbf19c1a451743e1c435a91e55d03e6714eb1
Vahe <ohanyanv703@gmail.com>
2026-04-19 20:13:34 +0400

    auth controller added google/github funcs

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,10 @@
+import process from "node:process";
 import bcrypt from "bcrypt";
 import connectDB from "../config/db.js";
 import { createToken } from "../utils/jwtToken.js";
+import { OAuth2Client } from "google-auth-library";
+
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 const sanitizeUsername = (value = "") =>
   value
@@ -135,4 +139,131 @@ const login = async (data) => {
   };
 };
 
-export { signUp, login };
+const googleAuth = async (data) => {
+  try {
+    const db = await connectDB();
+    const users = db.collection("users");
+    const usersStats = db.collection("usersStats");
+    const { tokenId } = data; // this maybe tokenId from front i need to check
+    const ticket = await client.verifyIdToken({
+      idToken: tokenId,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
+    const payload = ticket.getPayload();
+    const { email, given_name, family_name, sub: googleId } = payload;
+
+    let user = await users.findOne({ email });
+    if (!user) {
+      const baseUsername =
+        sanitizeUsername(given_name) ||
+        sanitizeUsername(`${given_name}${family_name}`) ||
+        "dev";
+      let finalUsername = `${baseUsername}_${uniqueSuffix()}`;
+      while (await users.findOne({ username: finalUsername })) {
+        finalUsername = `${baseUsername}_${uniqueSuffix()}`;
+      }
+      const result = await users.insertOne({
+        firstName: given_name,
+        lastName: family_name,
+        username: finalUsername,
+        email,
+        password: null,
+        googleId,
+        provider: "google",
+      });
+      await usersStats.insertOne(buildDefaultUserStats(result.insertedId));
+      user = await users.findOne({ _id: result.insertedId });
+    }
+
+    const token = createToken({ id: user._id });
+    return {
+      status: 200,
+      message: "Google Authentication Successful",
+      token,
+      userId: user._id,
+    };
+  } catch {
+    return {
+      status: 500,
+      message: "Google Authentication Failed",
+    };
+  }
+};
+
+const githubRedirect = (req, res) => {
+  const url = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&scope=user:email`;
+  res.redirect(url);
+};
+
+const githubCallback = async (req, res) => {
+  try {
+    const { code } = req.query;
+    const tokenResponse = await fetch(
+      "https://github.com/login/oauth/access_token",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          client_id: process.env.GITHUB_CLIENT_ID,
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          code,
+        }),
+      },
+    );
+
+    const tokenData = await tokenResponse.json();
+    const accessToken = tokenData.access_token;
+    const userRes = await fetch("https://api.github.com/user", {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    const userData = await userRes.json();
+    const emailRes = await fetch("https://api.github.com/user/emails", {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    const emailData = await emailRes.json();
+
+    const emails = Array.isArray(emailData) ? emailData : [];
+
+    const primaryEmail =
+      emails.find((e) => e.primary && e.verified)?.email || null;
+    const { id: githubId, login } = userData;
+
+    const db = await connectDB();
+    const users = db.collection("users");
+    const usersStats = db.collection("usersStats");
+    let user = await users.findOne({
+      $or: [{ githubId }, { email: primaryEmail }],
+    });
+    if (!user) {
+      const baseUsername = sanitizeUsername(login) || "dev";
+      let finalUsername = `${baseUsername}_${uniqueSuffix()}`;
+      while (await users.findOne({ username: finalUsername })) {
+        finalUsername = `${baseUsername}_${uniqueSuffix()}`;
+      }
+      const result = await users.insertOne({
+        username: finalUsername,
+        email: primaryEmail,
+        githubId,
+        provider: "github",
+        password: null,
+      });
+      await usersStats.insertOne(buildDefaultUserStats(result.insertedId));
+      user = { _id: result.insertedId };
+    }
+    const jwt = createToken({ id: user._id });
+    res.redirect(`http://localhost:5173/oauth-success?token=${jwt}`);
+  } catch (err) {
+    console.log(err);
+    res.redirect("http://localhost:5173/oauth-failure");
+  }
+};
+
+export { signUp, login, googleAuth, githubRedirect, githubCallback };
