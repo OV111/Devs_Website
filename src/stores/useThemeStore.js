06c0bb9ecaa4485892e4ba18fe76084da465bcff
Vahe <ohanyanv703@gmail.com>
2026-05-05 23:54:59 +0400

    minor fix

--- src/stores/useThemeStore.js
+++ src/stores/useThemeStore.js
@@ -3,7 +3,7 @@ import { create } from "zustand";
 const useThemeStore = create((set, get) => ({
   theme: localStorage.getItem("theme") || "dark",
   setTheme: () => {
-    const newTheme = get().theme === "dark" ? "light" : "dark";
+    const newTheme = get().theme === "dark" ? "dark" : "dark";
     set({ theme: newTheme });
     localStorage.setItem("theme", newTheme);
   },
