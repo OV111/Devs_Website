cd0776b149965f55b4282e9bc40046f490559458
Vahe <ohanyanv703@gmail.com>
2026-05-16 19:31:44 +0400

    add:constants of blog page

---
+++ constants/Blogs.js
@@ -0,0 +1,12 @@
+export const FILTER_TABS = [
+  "All",
+  "Latest",
+  "Popular",
+  "Trending",
+];
+
+export const SORT_OPTIONS = ["Newest", "Oldest", "Most Viewed"];
+
+export const DIFFICULTIES = ["Beginner", "Intermediate", "Advanced"];
+
+export const READ_TIMES = ["< 5 min", "5–10 min", "10+ min"];
